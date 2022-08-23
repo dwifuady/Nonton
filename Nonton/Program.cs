2721cd83649a203e293952345b7bcd9ff4dc98b8
S Dwi Fuady <sdwi@bizcover.com.au>
2022-08-23 18:54:40 +0700

    add SqliteWasmHelper and DbContext

--- Nonton/Program.cs
+++ Nonton/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 using Nonton;
 using Nonton.Services;
+using Nonton.Data;
+using SqliteWasmHelper;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +14,8 @@ builder.Services.AddMudServices();
 builder.Services.AddScoped<IAddonService, AddonService>();
 builder.Services.AddScoped<ICatalogService, CatalogService>();
 builder.Services.AddScoped<IMetaService, MetaService>();
+builder.Services.AddSqliteWasmDbContextFactory<NontonDbContext>(
+    opts => opts.UseSqlite("Data Source=nonton.sqlite3"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
