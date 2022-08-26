8a3865250d5170521a847b378e90180cab4a8582
dwifuady <dwifuady@gmail.com>
2022-08-26 09:26:15 +0700

    fix date issue on published site

--- Nonton/Data/NontonDbContext.cs
+++ Nonton/Data/NontonDbContext.cs
@@ -4,6 +4,12 @@ namespace Nonton.Data
 {
     public class NontonDbContext : DbContext
     {
+        /// <summary>
+        /// FIXME: This is required for EF Core 6.0 as it is not compatible with trimming.
+        /// </summary>
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+        private static Type _keepDateOnly = typeof(DateOnly);
+        
         public NontonDbContext(DbContextOptions<NontonDbContext> options): base(options)
         {
             
