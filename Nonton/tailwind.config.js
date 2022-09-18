746124dbeb1185baba09006b0ac674590328db1d
S Dwi Fuady <sdwi@bizcover.com.au>
2022-09-18 21:56:35 +0700

    setup tailwind and redesign mobile home page

---
+++ Nonton/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+    content: ["./**/*.{razor,html,cshtml}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                '.no-scrollbar::-webkit-scrollbar': {
+                    'display': 'none',
+                },
+                '.no-scrollbar': {
+                    '-ms-overflow-style': 'none',
+                    'scrollbar-width': 'none'
+                }
+            });
+        })
+    ]
+}
